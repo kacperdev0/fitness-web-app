6b09802e6bffe68a017350eb55cba2a942b941ba
KacperOldz <kobula@protonmail.com>
2024-04-07 20:33:32 +0200

    updated calculatorsView

--- src/CalculatorsView.js
+++ src/CalculatorsView.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import Slider from 'react-slider';
+import "./calculatorView.css"
 
 function CalculatorsView() {
-    const [heightValue, setHeightValue] = useState('');
+    const [heightValue, setHeightValue] = useState(170);
     const [weightValue, setWeightValue] = useState('');
     const [ageValue, setAgeValue] = useState('');
     const [bmiValue, setBmiValue] = useState('');
     const [calorieValue, setCalorieValue] = useState('');
-   
+
     const calculateBmiAndCalories = () => {
        if (heightValue && weightValue && ageValue) {
          const heightInMeters = heightValue / 100;
@@ -18,82 +20,56 @@ function CalculatorsView() {
        }
     };
    
-    const styles = {
-       container: {
-         display: 'flex',
-         flexDirection: 'column',
-         alignItems: 'center',
-         marginTop: '50px',
-       },
-       inputContainer: {
-         marginBottom: '20px',
-       },
-       input: {
-         marginTop: '10px',
-         padding: '10px',
-         fontSize: '16px',
-       },
-       calculateBtn: {
-         padding: '10px 20px',
-         fontSize: '16px',
-         cursor: 'pointer',
-         backgroundColor: '#007bff',
-         color: 'white',
-         border: 'none',
-         borderRadius: '5px',
-       },
-       result: {
-         marginTop: '20px',
-         fontSize: '18px',
-       },
-    };
-   
 
     return (
-        <div id="right-panel" style={{ padding: "2%", width: "91%", display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <div style={styles.container}>
-                <h1>BMI and Calorie Calculator</h1>
-                <div style={styles.inputContainer}>
-                    <label htmlFor="height">Enter Your Height (cm):</label>
-                    <input
-                    type="number"
-                    id="height"
-                    value={heightValue}
-                    onChange={(e) => setHeightValue(e.target.value)}
-                    style={styles.input}
-                    />
-                </div>
-                <div style={styles.inputContainer}>
-                    <label htmlFor="weight">Enter Your Weight (kg):</label>
-                    <input
-                    type="number"
-                    id="weight"
-                    value={weightValue}
-                    onChange={(e) => setWeightValue(e.target.value)}
-                    style={styles.input}
-                    />
-                </div>
-                <div style={styles.inputContainer}>
-                    <label htmlFor="age">Enter Your Age (years):</label>
-                    <input
-                    type="number"
-                    id="age"
-                    value={ageValue}
-                    onChange={(e) => setAgeValue(e.target.value)}
-                    style={styles.input}
-                    />
-                </div>
-                <button style={styles.calculateBtn} onClick={calculateBmiAndCalories}>
-                    Calculate BMI and Estimated Calories
-                </button>
-                {bmiValue && calorieValue && (
-                    <div style={styles.result}>
-                    <p>Your BMI: <span>{bmiValue}</span></p>
-                    <p>Estimated Daily Calories: <span>{calorieValue}</span></p>
-                    </div>
-                )}
-                </div>
+      <div id="right-panel" style={{ padding: "2%", width: "91%"}}>
+        <h1>BMI and Calorie Calculator</h1>
+          <div style={{ display: "flex"}}>
+
+            <div style={{width: "30%", marginLeft: "20%"}}>
+              <label htmlFor="height">Enter Your Height (cm):</label>
+                <Slider
+                  orientation="vertical"
+                  value={heightValue}
+                  onChange={(newValue) => setHeightValue(newValue)}
+                  min={100}
+                  max={250}
+                  invert={true}
+                  className="vertical-slider"
+                  renderThumb={(props, state) => <div {...props}><b>{state.valueNow}</b></div>}
+                />
+            </div>
+
+            <div style={{width: "30%", textAlign: "center", marginTop: "5%"}}>
+                <label htmlFor="weight">Enter Your Weight (kg):</label><br/>
+                <input
+                type="number"
+                id="weight"
+                value={weightValue}
+                onChange={(e) => setWeightValue(e.target.value)}
+                /><br/>
+
+                <label htmlFor="age" style={{marginTop: "100px"}}>Enter Your Age (years):</label><br/>
+                <input
+                type="number"
+                id="age"
+                value={ageValue}
+                onChange={(e) => setAgeValue(e.target.value)}
+                />
+          </div>  
         </div>
+        
+        <button onClick={calculateBmiAndCalories}>
+              Calculate BMI and Estimated Calories
+        </button>
+
+        {bmiValue && calorieValue && (
+              <div>
+              <p>Your BMI: <span>{bmiValue}</span></p>
+              <p>Estimated Daily Calories: <span>{calorieValue}</span></p>
+              </div>
+          )}
+      </div>
     );
 }
 
