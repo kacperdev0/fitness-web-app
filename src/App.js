0de7eb02eaf3eb4990fe9324fc24fd4a7b9f91d1
KacperOldz <kobula@protonmail.com>
2024-05-05 14:03:34 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Import Routes from react-router-dom
 import BodyView from "./BodyView";
 import CalculatorsView from "./CalculatorsView";
 import ArticlesView from "./ArticlesView";
@@ -13,7 +14,6 @@ import HomeIcon from '@mui/icons-material/Home';
 import CalculateIcon from '@mui/icons-material/Calculate';
 import ArticleIcon from '@mui/icons-material/Article';
 
-
 function App() {
  const [currentView, setCurrentView] = useState("home");
 
@@ -22,43 +22,47 @@ function App() {
  };
 
  return (
-    <div className="container">
-      <div className="bg" id="navigation-panel">
-        <Divider />
-          <List>
-            <ListItem key="Home" disablePadding  onClick={() => handleViewChange("home")}>
-              <ListItemButton>
-                <ListItemIcon>
-                    <HomeIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Home" />
-              </ListItemButton>
-            </ListItem>
+    <Router>
+      <div className="container">
+        <div className="bg" id="navigation-panel">
+          <Divider />
+            <List>
+              <ListItem key="Home" disablePadding>
+                <ListItemButton component={Link} to="/home" onClick={() => handleViewChange("home")}>
+                  <ListItemIcon>
+                      <HomeIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItem>
 
-            <ListItem key="Calculators" disablePadding onClick={() => handleViewChange("calculators")}>
-              <ListItemButton>
-                <ListItemIcon>
-                    <CalculateIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Calculators" />
-              </ListItemButton>
-            </ListItem>
+              <ListItem key="Calculators" disablePadding>
+                <ListItemButton component={Link} to="/calculators" onClick={() => handleViewChange("calculators")}>
+                  <ListItemIcon>
+                      <CalculateIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Calculators" />
+                </ListItemButton>
+              </ListItem>
 
-            <ListItem key="Aricles" disablePadding onClick={() => handleViewChange("articles")}>
-              <ListItemButton>
-                <ListItemIcon>
-                    <ArticleIcon/>
-                </ListItemIcon>
-                <ListItemText primary="Articles" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        <Divider /> 
+              <ListItem key="Articles" disablePadding>
+                <ListItemButton component={Link} to="/articles" onClick={() => handleViewChange("articles")}>
+                  <ListItemIcon>
+                      <ArticleIcon/>
+                  </ListItemIcon>
+                  <ListItemText primary="Articles" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          <Divider /> 
+        </div>
+        <Routes>
+          <Route path="/home" element={<BodyView />} />
+          <Route path="/calculators" element={<CalculatorsView />} />
+          <Route path="/articles" element={<ArticlesView />} />
+        </Routes>
       </div>
-      {currentView === "home" && <BodyView/>}
-      {currentView === "calculators" && <CalculatorsView/>}
-      {currentView === "articles" && <ArticlesView/>}
-    </div>
+    </Router>
  );
 }
 
