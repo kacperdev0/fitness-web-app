4320e2021e20d947528503a6abc00e87e8ecdf84
KacperOldz <kobula@protonmail.com>
2024-05-26 15:57:55 +0200

    bug fixes

--- src/Loading.js
+++ src/Loading.js
@@ -3,7 +3,7 @@ import ReactLoading from 'react-loading';
 
 function Loading() {
   return (
-    <div id="right-panel" style={{ padding: "2%", width: "91%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <div id="right-panel" style={{ padding: "2%", width: "96%", height: "92vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
         <ReactLoading type={"cubes"} color={"#1e67c7"} height={'10%'} width={'10%'} />
     </div>
   );
